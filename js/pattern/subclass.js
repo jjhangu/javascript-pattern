2b5c5d4c33878c97dd5f76f15110c42767e80d8a
Sanghwa Jung <jjhangu@naver.com>
2015-08-05 16:20:51 +0900

    addsubcalss

---
+++ js/pattern/subclass.js
@@ -0,0 +1,22 @@
+/**
+ * Created by ddavid on 2015-08-05.
+ */
+
+var Person = function(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.gender = 'male';
+};
+
+var clark = new Person("Clark", "Kent");
+
+var Superhero = function(firstName, lastName, powers){
+
+    Person.call(this, firstName, lastName);
+
+    this.powers = powers;
+}
+
+Superhero.prototype = Object.create(Person.prototype);
+var superman = new Superhero("Clark", "Kent", ['flight', 'heat-vision']);
+console.log(superman);
